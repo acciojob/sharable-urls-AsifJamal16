5905e0cec34de5804a6eae71e3f80568b5b039f5
AsifJamal16 <asifjamal582005@gmail.com>
2025-10-29 12:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// your code here
+
+
+    let baseURL = "https://localhost:8080/";
+    let name = document.getElementById("name").value.trim();
+    let year = document.getElementById("year").value.trim();
+
+    let query = [];
+
+    if (name !== "") {
+        query.push("name=" + encodeURIComponent(name));
+    }
+
+    if (year !== "") {
+        query.push("year=" + encodeURIComponent(year));
+    }
+
+    if (query.length > 0) {
+        baseURL += "?" + query.join("&");
+    }
+
+    document.getElementById("url").innerText = baseURL;
+});
+
